2c9eb4bbc905f0af1f5a9f40a12d6eb9ee1ae7ae
Rutik <rutikkulkarni2001@gmail.com>
2023-12-22 19:38:14 +0530

    ME_QTIFY_MODULE_GRID uplode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import { StyledEngineProvider } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { fetchNewAlbums, fetchSongs, fetchTopAlbums } from "./Api/Api";
 
 function App() {
+  const [searchData, useSearchData] = useState();
+  const [data, setdata] = useState({});
+
+  const generatData = (key, source) => {
+    source().then((data) => {
+      setdata((prevData) => {
+        return {
+          ...prevData,
+          [key]: data,
+        };
+      });
+    });
+  };
+
+  useEffect(() => {
+    generatData("topAlbums", fetchTopAlbums);
+    generatData("newAlbums", fetchNewAlbums);
+    generatData("songs", fetchSongs);
+  }, []);
+
+  const { topAlbums = [], newAlbums = [], songs = [] } = data;
+
   return (
     <>
-      <Navbar searchData={""}/> 
-      <Hero />
+      {/* <Navbar searchData={""}/> 
+      <Hero /> */}
+      <StyledEngineProvider injectFirst>
+        <Navbar searchData={[...topAlbums, ...newAlbums]} />
+        <Outlet context={{ data: { topAlbums, newAlbums, songs } }} />
+      </StyledEngineProvider>
     </>
   );
 }
