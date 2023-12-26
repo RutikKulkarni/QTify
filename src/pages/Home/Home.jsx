b275d1ef106c2702a393f141d0cf1b89cc93e177
Rutik <rutikkulkarni2001@gmail.com>
2023-12-26 22:06:58 +0530

    Songs and their Tabs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import { useOutletContext } from "react-router-dom";
 import Section from "../../components/Section/Section";
 import styles from "./Home.module.css";
 import Hero from "../../components/Hero/Hero";
+import { fetchFilters } from "../../Api/Api";
 
 function Home({}) {
   const { data } = useOutletContext();
@@ -13,7 +14,7 @@ function Home({}) {
       <div className={styles.wrapper}>
         <Section title="Top Albums" data={topAlbums} type="album" />
         <Section title="New Albums" data={newAlbums} type="album" />
-        {/* <Section title="Songs" data={songs} type="song" /> */}
+        <Section title="Songs" data={songs} filterSource={fetchFilters} type="song" />
       </div>
     </>
   );
